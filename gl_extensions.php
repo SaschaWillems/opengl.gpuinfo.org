e9c76b0db34acf8693e4594d6e9a63193d79f0b3
saschawillems <webmaster@saschawillems.de>
2015-01-05 23:30:20 +0100

    Extension listing : First (not yet finished) version with live search (using dataTables) and bootstrap (visuals)

--- gl_extensions.php
+++ gl_extensions.php
@@ -1,3 +1,13 @@
+<head>
+	<link rel="stylesheet" href="./libs/jquery-ui/themes/flick/jquery-ui.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//cdn.datatables.net/plug-ins/3cfcc339e89/integration/bootstrap/3/dataTables.bootstrap.css">	
+	<script src="./libs/jquery.min.js"></script>
+	<script src="./libs/jquery-ui/jquery-ui.min.js"></script>
+	<script src="//cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
+	<script src="//cdn.datatables.net/plug-ins/3cfcc339e89/integration/bootstrap/3/dataTables.bootstrap.js"></script>
+</head>
+
 <?php 
 	/* 		
 		*
@@ -28,135 +38,116 @@
 
 <div id="content">
 	
-	<table border=0>
-		<tr><td class="firstrow">
-			<form method="get" action="" style="margin-bottom:0px;">
-				Filter by extension name (like) : 
-				<input type="text" name="searchstring" size=40">      
-				<input type="submit" value="Filter">
-			</form>     
-		</tr></td>			
-	</table>		
-	
-	<table border="0">
-		<TBODY>    
-			<?php
-				
-				// Filter via input box
-				$searchstring = '';
-				if(isset($_GET['searchstring'])) {	 
-					$searchstring  = mysql_real_escape_string(strtolower($_GET['searchstring']));
-				} 	
-				$search = '';
-				if ($searchstring != '') {
-					$search = ' where name like "%'.$searchstring.'%"';
+	<table border="0" id="extensions" class="table table-striped table-bordered" cellspacing="0" width="100%">
+		<?php
+			
+			// Filter via input box
+			$searchstring = '';
+			if(isset($_GET['searchstring'])) {	 
+				$searchstring  = mysql_real_escape_string(strtolower($_GET['searchstring']));
+			} 	
+			$search = '';
+			if ($searchstring != '') {
+				$search = ' where name like "%'.$searchstring.'%"';
+			}			
+			
+			// Get total report count for percentage statistics
+			$str             = "SELECT count(*) FROM openglcaps";	  	   
+			$sqlresult       = mysql_query($str) or die(mysql_error());  
+			$totalnumreports = mysql_result($sqlresult, 0);
+			
+			$str = "SELECT PK FROM openglextensions $search";
+			$sqlresult = mysql_query($str) or die(mysql_error());  
+			
+			echo "<thead><tr>";  
+			
+			$sortby = $_GET['sortby'];				
+			if ($sortby == "extDesc") {
+				echo "<td class='caption'><b><a href='".$_SERVER['PHP_SELF']."?sortby=extAsc'>Extension</a></b></td>";		   
+				$sortOrder = 'DESC';
+				} else {
+				echo "<td class='caption'><b><a href='".$_SERVER['PHP_SELF']."?sortby=extDesc'>Extension</a></b></td>";		   
+				$sortOrder = 'ASC';
+			}
+			
+			echo "<td class='caption'><b>Count</b></td>";		   
+			echo "<td class='caption'><b><a href='".$_SERVER['PHP_SELF']."?sortby=percentage'>Percentage</a></b></td>";		   
+			echo "</tr></thead><tbody>";
+			
+			
+			// Gather all extensions 
+			$extarray = array();  
+			while($row = mysql_fetch_row($sqlresult))
+			{	
+				foreach ($row as $data)
+				{
+					$extarray[] = $data;	 
 				}
-				
-				// Caption
-				echo "<tr><td id='tableheader' colspan=4><b>";
-				$sqlresult = mysql_query("SELECT count(*) FROM openglextensions $search") or die(mysql_error());  	
-				$sqlcount  = mysql_result($sqlresult, 0);
-				if ($search != '') {
-					echo "Listing $sqlcount OpenGL extensions (Name like '$searchstring')\n";
-					} else {
-					echo "Listing $sqlcount OpenGL extensions\n";
+			}   
+			
+			$str = "SELECT DISTINCT Name FROM openglgpuandext LEFT JOIN openglextensions ON openglextensions.PK = openglgpuandext.ExtensionID $search";
+			if ($sortby == "percentage") {
+				$str .= "ORDER BY FIELD(SUBSTR(openglextensions.Name, 1, 3), 'GL_') DESC, FIELD(SUBSTR(openglextensions.Name, INSTR(openglextensions.Name, '_')+1, 3), 'EXT', 'ARB') DESC, openglextensions.Name $sortOrder";
+			}
+			$sqlresult = mysql_query($str) or die(mysql_error());  ; 
+			$extname = array();
+			$totalextcount = 0;
+			while($row = mysql_fetch_row($sqlresult))
+			{	
+				foreach ($row as $data)
+				{
+					$extname[] = $data;	 
 				}
-				echo "</b></td></tr>\n";		  
+			}  
+			$extcount = array();
+			
+			$index = 0;	
+			foreach ($extarray as $ext) {	  					
+				$substr = "SELECT * FROM openglgpuandext LEFT JOIN openglextensions ON openglextensions.PK = openglgpuandext.ExtensionID where openglgpuandext.ExtensionID = $ext";	
+				$substr = "SELECT COUNT(*) FROM openglgpuandext WHERE openglgpuandext.ExtensionID = $ext";
+				$subsqlresult = mysql_query($substr) or die(mysql_error());  ; 
 				
+				//$count = mysql_num_rows($subsqlresult); 
 				
-				// Get total report count for percentage statistics
-				$str             = "SELECT count(*) FROM openglcaps";	  	   
-				$sqlresult       = mysql_query($str) or die(mysql_error());  
-				$totalnumreports = mysql_result($sqlresult, 0);
+				while($row = mysql_fetch_array($subsqlresult)){
+					$extcount[] = $row['COUNT(*)'];
+				}	
 				
-				$str = "SELECT PK FROM openglextensions $search";
-				$sqlresult = mysql_query($str) or die(mysql_error());  
-				
-				echo "<tr>";  
-				
-				$sortby = $_GET['sortby'];				
-				if ($sortby == "extDesc") {
-					echo "<td class='caption'><b><a href='".$_SERVER['PHP_SELF']."?sortby=extAsc'>Extension</a></b></td>";		   
-					$sortOrder = 'DESC';
-					} else {
-					echo "<td class='caption'><b><a href='".$_SERVER['PHP_SELF']."?sortby=extDesc'>Extension</a></b></td>";		   
-					$sortOrder = 'ASC';
-				}
-							
-				echo "<td class='caption'><b>Count</b></td>";		   
-				echo "<td class='caption'><b><a href='".$_SERVER['PHP_SELF']."?sortby=percentage'>Percentage</a></b></td>";		   
-				echo "</tr>";
-				
-				
-				// Gather all extensions 
-				$extarray = array();  
-				while($row = mysql_fetch_row($sqlresult))
-				{	
-					foreach ($row as $data)
-					{
-						$extarray[] = $data;	 
-					}
-				}   
-				
-				$str = "SELECT DISTINCT Name FROM openglgpuandext LEFT JOIN openglextensions ON openglextensions.PK = openglgpuandext.ExtensionID $search";
-				if ($sortby !== "percentage") {
-					$str .= "ORDER BY FIELD(SUBSTR(openglextensions.Name, 1, 3), 'GL_') DESC, FIELD(SUBSTR(openglextensions.Name, INSTR(openglextensions.Name, '_')+1, 3), 'EXT', 'ARB') DESC, openglextensions.Name $sortOrder";
-				}
-				$sqlresult = mysql_query($str) or die(mysql_error());  ; 
-				$extname = array();
-				$totalextcount = 0;
-				while($row = mysql_fetch_row($sqlresult))
-				{	
-					foreach ($row as $data)
-					{
-						$extname[] = $data;	 
-					}
-				}  
-				$extcount = array();
-				
-				$index = 0;	
-				foreach ($extarray as $ext) {	  					
-					$substr = "SELECT * FROM openglgpuandext LEFT JOIN openglextensions ON openglextensions.PK = openglgpuandext.ExtensionID where openglgpuandext.ExtensionID = $ext";	
-					$substr = "SELECT COUNT(*) FROM openglgpuandext WHERE openglgpuandext.ExtensionID = $ext";
-					$subsqlresult = mysql_query($substr) or die(mysql_error());  ; 
-				
-					//$count = mysql_num_rows($subsqlresult); 
-					
-					while($row = mysql_fetch_array($subsqlresult)){
-						$extcount[] = $row['COUNT(*)'];
-					}	
-					
+				$index++;
+			} 
+			
+			
+			// Output table
+			$index = 0;
+			for ($i = 0; $i <= count($extname); $i++) {
+				if (!empty($extname[$i])) {
+					$link = str_replace("GL_", "", $extname[$i]);
+					$extparts = explode("_", $link);
+					$vendor = $extparts[0];
+					$link = str_replace($vendor."_", "", $link);						
+					echo "<tr>";						
+					echo "<td class='firstrow'><a href='listreports2.php?listreportsbyextension=".$extname[$i]."'>".$extname[$i]."</a> (<a href='listreports2.php?listreportsbyextensionunsupported=".$extname[$i]."'>not</a>) [<a href='http://www.opengl.org/registry/specs/$vendor/$link.txt' target='_blank' title='Show specification for this extensions'>?</a>]</td>";
+					echo "<td class='firstrow' align=center>".$extcount[$i]."</td>";
+					echo "<td class='firstrow' align=center>".round(($extcount[$i]/$totalnumreports*100), 2)."%</td>";
+					echo "</tr>";	    
 					$index++;
-				} 
-				
-				//  Sort by supported percentage
-				if ($sortby == "percentage") {
-					array_multisort($extcount, SORT_DESC, $extname); 
-				}								
-				
-				// Output table
-				$index = 0;
-				for ($i = 0; $i <= count($extname); $i++) {
-					if (!empty($extname[$i])) {
-						$bgcolor  = $index % 2 != 0 ? $bgcolordef : $bgcolorodd; 
-						$link = str_replace("GL_", "", $extname[$i]);
-						$extparts = explode("_", $link);
-						$vendor = $extparts[0];
-						$link = str_replace($vendor."_", "", $link);						
-						echo "<tr style='background-color:$bgcolor;'>";						
-						echo "<td class='firstrow'><a href='listreports2.php?listreportsbyextension=".$extname[$i]."'>".$extname[$i]."</a> (<a href='listreports2.php?listreportsbyextensionunsupported=".$extname[$i]."'>not</a>) [<a href='http://www.opengl.org/registry/specs/$vendor/$link.txt' target='_blank' title='Show specification for this extensions'>?</a>]</td>";
-						echo "<td class='firstrow' align=center>".$extcount[$i]."</td>";
-						echo "<td class='firstrow' align=center>".round(($extcount[$i]/$totalnumreports*100), 2)."%</td>";
-						echo "</tr>";	    
-						$index++;
-					}
-				}  
-				
-				mysql_close();	
-			?>   
-		</tbody>	
-	</table>  
-	
+				}
+			}  
+			
+			
+			dbDisconnect();	
+		?>   
+	</tbody>
+</table>  
+
+<script>
+	$(document).ready(function() {
+		$('#extensions').DataTable({
+			"pageLength" : 50
+		});
+	} );	
+</script>
+
 <?php include("./gl_footer.inc");	?></center>	
 </div>
 </body>
