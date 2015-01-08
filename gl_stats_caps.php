c0ba3999fac168c0da073353502635ae32a7922b
saschawillems <webmaster@saschawillems.de>
2015-01-08 22:00:05 +0100

    Caps listing : Using data tables and live search

--- gl_stats_caps.php
+++ gl_stats_caps.php
@@ -1,22 +1,35 @@
- <?php 
+<head>
+	<link rel="stylesheet" href="./libs/jquery-ui/themes/flick/jquery-ui.css">
+	<link rel="stylesheet" href="./libs/bootstrap.min.css">
+	<link rel="stylesheet" href="./libs/dataTables.bootstrap.css">	
+	<link rel="stylesheet" href="./libs/dataTables.searchHighlight.css">	
+	<script src="./libs/jquery.min.js"></script>
+	<script src="./libs/jquery-ui/jquery-ui.min.js"></script>
+	<script src="./libs/jquery.highlight.js"></script>
+	<script src="./libs/jquery.dataTables.min.js"></script>
+	<script src="./libs/dataTables.bootstrap.js"></script>
+	<script src="./libs/dataTables.searchHighlight.min.js"></script>
+</head>
+
+<?php 
 	/* 		
-	*
-	* OpenGL hardware capability database server implementation
-	*	
-	* Copyright (C) 2011-2015 by Sascha Willems (www.saschawillems.de)
-	*	
-	* This code is free software, you can redistribute it and/or
-	* modify it under the terms of the GNU Affero General Public
-	* License version 3 as published by the Free Software Foundation.
-	*	
-	* Please review the following information to ensure the GNU Lesser
-	* General Public License version 3 requirements will be met:
-	* http://www.gnu.org/licenses/agpl-3.0.de.html
-	*	
-	* The code is distributed WITHOUT ANY WARRANTY; without even the
-	* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-	* PURPOSE.  See the GNU AGPL 3.0 for more details.		
-	*
+		*
+		* OpenGL hardware capability database server implementation
+		*	
+		* Copyright (C) 2011-2015 by Sascha Willems (www.saschawillems.de)
+		*	
+		* This code is free software, you can redistribute it and/or
+		* modify it under the terms of the GNU Affero General Public
+		* License version 3 as published by the Free Software Foundation.
+		*	
+		* Please review the following information to ensure the GNU Lesser
+		* General Public License version 3 requirements will be met:
+		* http://www.gnu.org/licenses/agpl-3.0.de.html
+		*	
+		* The code is distributed WITHOUT ANY WARRANTY; without even the
+		* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+		* PURPOSE.  See the GNU AGPL 3.0 for more details.		
+		*
 	*/
 	
 	include './gl_htmlheader.inc';	
@@ -25,79 +38,56 @@
 	
 	dbConnect();	 
 ?>
-   
+
 <div id="content">
-
-	 <table border=0>
-		<tr><td class="firstrow">
-			<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" style="margin-bottom:0px;">
-				Filter by capability name (like) : 
-				<input type="text" name="searchstring" size=40">      
-				<input type="submit" name="submit" value="Filter">
-			</form>     
-		</tr></td>			
-	</table>	
-<?php	
-
-	// Filter via input box
-	$searchstring = '';
-	if(isset($_POST['submit'])) {	 
-		$searchstring  = mysql_real_escape_string(strtolower($_POST['searchstring']));
-	} 	
 	
-	$str             = "SELECT * FROM openglcaps WHERE ReportID = 1";	  	   
-	$sqlresult       = mysql_query($str) or die(mysql_error());  	
- 
-  echo "<table border=0>";
-	if ($searchstring != '') {
-		echo "<tr><td id='tableheader'><b>OpenGL hardware capabilities (Name like '$searchstring')</td></tr>\n";
-	} else {
-		echo "<tr><td id='tableheader'><b>OpenGL hardware capabilities</td></tr>";
-	}	    
+	<table border="0" id="caps" class="table table-striped table-bordered" cellspacing="0" width="100%">
+		<thead><tr><td class="caption">OpenGL implementation capability</td></tr></thead>
+		
+		<?php		
+			$sqlresult = mysql_query("SELECT * FROM openglcaps WHERE ReportID = 1") or die(mysql_error());  	
+			
+			$colindex = 0;
+			$glcapnames = array();
+			$skipFields = array('GL_VENDOR', 'GL_VERSION', 'GL_RENDERER');
+			while($row = mysql_fetch_row($sqlresult))
+			{
+				foreach ($row as $data)
+				{   
+					$caption = mysql_field_name($sqlresult, $colindex);
+					$colindex++;	 
+					if (in_array($caption, $skipFields)) {
+						continue;
+					}
+					if ($searchstring != '') {
+						if (stripos($caption, $searchstring) === FALSE) {
+							continue;
+						}
+					}
+					if (strpos($caption, 'GL_') !== FALSE) {
+						$glcapnames[] = $caption;
+					}
+				}
+			}
+			
+			foreach ($glcapnames as $glcapname) {
+				echo "<tr><td class='firstrow'><a href='gl_stats_caps_single.php?listreportsbycap=$glcapname'>$glcapname</a></td></tr>";
+			}
+			
+			dbDisconnect();	
+		?>   
+	</tbody>
+</table>  
 
-  //while($report = mysql_fetch_object($sqlresult)) 
-//   {   
-   $colindex = 0;
-   $glcapnames = array();
-   while($row = mysql_fetch_row($sqlresult))
-    {
-	 foreach ($row as $data)
-	  {   
-	  $caption = mysql_field_name($sqlresult, $colindex);
-	  $colindex++;	 
-	   if ($caption === 'GL_VENDOR') {
-	    continue; }
-	   if ($caption === 'GL_RENDERER') {
-	    continue; }
-	   if ($caption === 'GL_VERSION') {
-	    continue; }
-	  if ($searchstring != '') {
-		if (stripos($caption, $searchstring) === FALSE) {
-			continue;
-		}
-	  }
-	  if (strpos($caption, 'GL_') !== FALSE) {
-		 $glcapnames[] = $caption;
-	  }
-     }
-//   break;
-  // }
-  
-   }
-   
-	sort($glcapnames); 
-	$index = 0;
-	foreach ($glcapnames as $glcapname) {
-		$bgcolor  = $index % 2 != 0 ? $bgcolordef : $bgcolorodd;   
-		$index++;
-		echo "<tr><td class='firstrow' style='padding-left:10px; font-size: 12px; background-color:".$bgcolor."'><a href='gl_stats_caps_single.php?listreportsbycap=$glcapname'>$glcapname</a></td></tr>";
-	}
-
-	echo "</table>";
-    
-	mysql_close();	?>
-  
-</p>
+<script>
+	$(document).ready(function() {
+		$('#caps').DataTable({
+			"pageLength" : 50,
+			"searchHighlight" : true,		
+			"lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
+		});
+	} );	
+</script>
 <?php include("./gl_footer.inc");	?>
 </div>
 </body>
