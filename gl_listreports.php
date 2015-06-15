a7e797af5feed86dfb283a1fd5af049f9335c5ad
saschawillems <webmaster@saschawillems.de>
2015-06-15 22:02:43 +0200

    Filtering dropdowns for different columns

--- gl_listreports.php
+++ gl_listreports.php
@@ -1,8 +1,8 @@
 <head>
 	<link rel="stylesheet" href="./libs/jquery-ui/themes/flick/jquery-ui.css">
 	<link rel="stylesheet" href="./libs/bootstrap.min.css">
-	<link rel="stylesheet" href="./libs/dataTables.bootstrap.css">	
-	<link rel="stylesheet" href="./libs/dataTables.searchHighlight.css">	
+	<link rel="stylesheet" href="./libs/dataTables.bootstrap.css">
+	<link rel="stylesheet" href="./libs/dataTables.searchHighlight.css">
 	<script src="./libs/jquery.min.js"></script>
 	<script src="./libs/jquery-ui/jquery-ui.min.js"></script>
 	<script src="./libs/jquery.highlight.js"></script>
@@ -11,98 +11,98 @@
 	<script src="./libs/dataTables.searchHighlight.min.js"></script>
 </head>
 
-<?php 
-	/* 		
+<?php
+	/*
 		*
 		* OpenGL hardware capability database server implementation
-		*	
+		*
 		* Copyright (C) 2011-2015 by Sascha Willems (www.saschawillems.de)
-		*	
+		*
 		* This code is free software, you can redistribute it and/or
 		* modify it under the terms of the GNU Affero General Public
 		* License version 3 as published by the Free Software Foundation.
-		*	
+		*
 		* Please review the following information to ensure the GNU Lesser
 		* General Public License version 3 requirements will be met:
 		* http://www.gnu.org/licenses/agpl-3.0.de.html
-		*	
+		*
 		* The code is distributed WITHOUT ANY WARRANTY; without even the
 		* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-		* PURPOSE.  See the GNU AGPL 3.0 for more details.		
+		* PURPOSE.  See the GNU AGPL 3.0 for more details.
 		*
 	*/
-	
-	include './gl_htmlheader.inc';	
+
+	include './gl_htmlheader.inc';
 	include './gl_menu.inc';
 	include './gl_config.php';
-	
-	dbConnect();	
-	
+
+	dbConnect();
+
 	function quickstats()
 	{
-		$sqlResult = mysql_query("SELECT count(*) FROM openglcaps");  	   
+		$sqlResult = mysql_query("SELECT count(*) FROM openglcaps");
 		$sqlCount = mysql_result($sqlResult, 0);
 		echo "Listing $sqlCount Reports\n";
-	}	 
+	}
 ?>
 
 
 <div id="content">
-	
+
 	<form method="get" action="gl_comparereports.php?compare" style="margin-bottom:0px;">
-		
+
 		<table  border="0" id="reports" class="table table-striped table-bordered" cellspacing="0" width="100%">
-			<?php					
-				
+			<?php
+
 				// Submitter
 				if($_GET['submitter'] != '') {
 					$submitter = mysql_real_escape_string(strtolower($_GET['submitter']));
-					echo "<caption class='tableheader'>Reports submitted by <b>$submitter</b></class>";	 
-				}	
-				
+					echo "<caption class='tableheader'>Reports submitted by <b>$submitter</b></class>";
+				}
+
 				$negate = false;
 				$caption = "";
-				
+
 				$colspan = 7;
 				$searchType = '';
-				
+
 				// External search (e.g. via statistics page)
 				if($_GET['listreportsbyextension'] != '') {
 					$searchstring  = mysql_real_escape_string(strtolower($_GET['listreportsbyextension']));
 					$searchType = 'extension';
 				}
-				
+
 				if($_GET['listreportsbyextensionunsupported'] != '') {
 					$searchstring  = mysql_real_escape_string(strtolower($_GET['listreportsbyextensionunsupported']));
-					$negate = true;										
+					$negate = true;
 					$searchType = 'extension';
 				}
-				
+
 				if($_GET['compressedtextureformat'] != '') {
 					$searchstring  = mysql_real_escape_string(strtolower($_GET['compressedtextureformat']));
 					$searchType = 'compressedformat';
-				}				
-				
-				if ($searchstring != '')	 
+				}
+
+				if ($searchstring != '')
 				{
 					echo "<caption class='tableheader'>";
 					if ($negate == false) {
-						echo "Displaying all reports supporting <b>".strtoupper($searchstring)." </b>";	   	  
+						echo "Displaying all reports supporting <b>".strtoupper($searchstring)." </b>";
 					}
 					if ($negate == true) {
-						echo "Displaying all reports not supporting <b>".strtoupper($searchstring)." </b>";	 
+						echo "Displaying all reports not supporting <b>".strtoupper($searchstring)." </b>";
 					}
-					echo "</caption>";		  
-				}	 
-				
-				
-				if (($searchstring == '') and ($submitter == '')) {   
-					echo "<caption class='tableheader'><b>";				
-					quickstats();  
-					echo "</b></class>";		  
-				} 
-				
-				
+					echo "</caption>";
+				}
+
+
+				if (($searchstring == '') and ($submitter == '')) {
+					echo "<caption class='tableheader'><b>";
+					quickstats();
+					echo "</b></class>";
+				}
+
+
 				// Normal listing without vendor specific headers
 				echo "<thead><tr>";
 				echo "	<td class='caption'>Renderer</td>";
@@ -113,100 +113,101 @@
 				echo "	<td class='caption'>OS</td>";
 				echo "	<td class='caption'>Date</td>";
 				echo "	<td class='caption' align=center><input type='submit' name='compare' value='compare'></td>";
-				echo "</tr></thead><tbody>"; 
-				
-				$str = "SELECT *, date(submissiondate) as reportdate, contextTypeName(contexttype) as ctxType FROM openglcaps ORDER BY reportid desc";	  	   
-				
+				echo "</tr></thead><tbody>";
+
+				$str = "SELECT *, date(submissiondate) as reportdate, contextTypeName(contexttype) as ctxType FROM openglcaps ORDER BY reportid desc";
+
 				if ($submitter != '') {
-					$str = "SELECT *, date(submissiondate) as reportdate, contextTypeName(contexttype) as ctxType FROM openglcaps where submitter = '$submitter' order by ReportID desc";	  	   
+					$str = "SELECT *, date(submissiondate) as reportdate, contextTypeName(contexttype) as ctxType FROM openglcaps where submitter = '$submitter' order by ReportID desc";
 				}
-				
+
 				// Search by compressed texture format support
 				if ( ($searchType == 'compressedformat')  && ($searchstring != '') ) {
 					echo "<b>compressedtextureformat</b>";
 					$str  = "SELECT *, date(submissiondate) as reportdate, contextTypeName(contexttype) as ctxType FROM openglcaps where";
 					$str .= " reportid in (select reportid from compressedTextureFormats where formatEnum =";
-					$str .= " (select enum from enumTranslationTable where text = '$searchstring')) ORDER BY reportid desc";	  	   						
-				}				
-				
-				$sqlresult = mysql_query($str); 
-				
+					$str .= " (select enum from enumTranslationTable where text = '$searchstring')) ORDER BY reportid desc";
+				}
+
+				$sqlresult = mysql_query($str);
+
 				while($row = mysql_fetch_object($sqlresult))
 				{
-					$description = trim($row->description);		
-					$reportid    = trim($row->ReportID);	 
+					$description = trim($row->description);
+					$reportid    = trim($row->ReportID);
 					$vendor	  = trim($row->GL_VENDOR);
 					$renderer	  = trim($row->GL_RENDERER);
 					$submissiondate = trim($row->reportdate);
 					$os          = trim($row->os);
-					$ctxtype = trim($row->ctxType);					
-					
+					$ctxtype = trim($row->ctxType);
+
 					// Remove certain unnecessary strings from version info (e.g. "compatibility context for ATI"
 					$versionreplace = array("Compatibility Profile Context");
 					$version = str_replace($versionreplace, "", trim($row->GL_VERSION));
-					$glslsversion = trim($row->GL_SHADING_LANGUAGE_VERSION); 
-					
+					$glslsversion = trim($row->GL_SHADING_LANGUAGE_VERSION);
+
 					// Search by extension support
-					if ( ($searchstring != '') && ($searchType == 'extension') ) {	 
-						$str = "SELECT Name FROM openglgpuandext LEFT JOIN openglextensions ON openglextensions.PK = openglgpuandext.ExtensionID WHERE openglgpuandext.ReportID = $reportid";			
-						
-						$sqlsubresult = mysql_query($str);	 
+					if ( ($searchstring != '') && ($searchType == 'extension') ) {
+						$str = "SELECT Name FROM openglgpuandext LEFT JOIN openglextensions ON openglextensions.PK = openglgpuandext.ExtensionID WHERE openglgpuandext.ReportID = $reportid";
+
+						$sqlsubresult = mysql_query($str);
 						$subarray = array();
-						
+
 						while($subrow = mysql_fetch_row($sqlsubresult))
-						{	
+						{
 							foreach ($subrow as $data)
 							{
-								$subarray[] = strtolower($data);	  
+								$subarray[] = strtolower($data);
 							}
-						}	 		  
-						
-						if ($negate == true) {
-							if (in_array($searchstring, $subarray)) { continue; } 
 						}
-						
+
+						if ($negate == true) {
+							if (in_array($searchstring, $subarray)) { continue; }
+						}
+
 						if ($negate == false) {
-							if (!in_array($searchstring, $subarray)) { continue; } 
-						} 
+							if (!in_array($searchstring, $subarray)) { continue; }
+						}
 					}
-					
+
 					// Extract version numbers
-					preg_match("|[0-9]+(?:\.[0-9]*)?|", $version, $versionint);	 
-					preg_match("|[0-9]+(?:\.[0-9]*)?|", $glslsversion, $glslsversionint);	 
-					
+					preg_match("|[0-9]+(?:\.[0-9]*)?|", $version, $versionint);
+					preg_match("|[0-9]+(?:\.[0-9]*)?|", $glslsversion, $glslsversionint);
+
 					echo "<tr>";
-					echo "	<td class='firstrow'><a href='gl_generatereport.php?reportID=$reportid'>$renderer</a></td>";	
-					echo "	<td class='valuezeroleft'>".$version."</td>";	 
+					echo "	<td class='firstrow'><a href='gl_generatereport.php?reportID=$reportid'>$renderer</a></td>";
+					echo "	<td class='valuezeroleft'>".$version."</td>";
 					echo "	<td class='valuezeroleft''>".$versionint[0]."</td>\n";
 					echo "	<td class='valuezeroleft'>".$glslsversionint[0]."</td>";
-					echo "	<td class='valuezeroleft'>".$ctxtype."</td>";		 		 
+					echo "	<td class='valuezeroleft'>".$ctxtype."</td>";
 					echo "	<td class='valuezeroleft'>$os</td>";
-					echo "	<td class='valuezeroleft'>$submissiondate</td>";	 
+					echo "	<td class='valuezeroleft'>$submissiondate</td>";
 					echo "	<td align='center'><input type='checkbox' name='id[$reportid]'></td>";
 					echo "</tr>";
-					
+
 				}
-				
-				dbDisconnect();  
-			?>   
+
+				dbDisconnect();
+			?>
 		</tbody>
 		</table>
-		
-		
-	</form>   
-	
+
+
+	</form>
+
 	<script>
 		$(document).ready(function() {
 			$('#reports').DataTable({
 				"order": [[ 6, "desc" ]],
+				"deferRender": true,
 				"pageLength" : 50,
-				"stateSave": true, 	
+				"stateSave": true,
 				"searchHighlight": true,
 				"lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
-				
+
 				initComplete: function () {
 					var api = this.api();
-					
+
 					api.columns().indexes().flatten().each( function ( i ) {
 						if ((i>1) && (i<6)) {
 							var column = api.column( i );
@@ -216,23 +217,23 @@
 								var val = $.fn.dataTable.util.escapeRegex(
 								$(this).val()
 								);
-								 
+
 								column
 								.search( val ? '^'+val+'$' : '', true, false )
 								.draw();
 							} );
-							
+
 							column.data().unique().sort().each( function ( d, j ) {
 								select.append( '<option value="'+d+'">'+d+'</option>' )
 							} );
 						};
 					} );
-				}			
-				
+				}
+
 			});
-		} );	
-	</script>	 
-	
+		} );
+	</script>
+
 	<?php include("./gl_footer.inc");	?>
 </div>
 
