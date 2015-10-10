ca71cf4d498bd0e8bc1a97129c49024e79d16f76
saschawillems <webmaster@saschawillems.de>
2015-10-10 14:22:11 +0200

    Donate on about page, download link in navigation

--- gl_about.php
+++ gl_about.php
@@ -22,18 +22,31 @@
 ?>
 
 <div id='reportdiv'>	   
-<div class="container">
-    <div class="jumbotron">    
-        <h1>About this database</h1>
-        <p>
-            Welcome to the public (inofficial) OpenGL hardware database, an online tool for developers that want to check out the OpenGL hardware capabilites for the different<br>
-            graphics cards, operating systems and driver versions out there.<br><br>
-            This database and the tools to fill it are developed and maintained by me (<a href="http://www.saschawillems.de/" target="_blank">Sascha Willems</a>) in my spare time.<br>
-            No profit is made from this data, nor is this data used in any commercial way and no personal data is transferred, stored or passed.<br><br>
-            If you want to contribute to the development, you can find the source code at <a href="https://github.com/SaschaWillems" target="_blank">https://github.com/SaschaWillems</a>.
-        </p>
-    </div>    
-</div>
+	<div class="panel panel-default">
+		<div class="panel-body" style="margin-left:50px; width:65%px;">    
+			<h2>About this database</h2>
+			<div>
+				Welcome to the public (inofficial) OpenGL hardware database, an online tool for developers that want to check out the OpenGL hardware capabilites for the different<br>
+				graphics cards, operating systems and driver versions out there.<br><br>
+				This database and the tools to fill it are developed and maintained by me (<a href="http://www.saschawillems.de/" target="_blank">Sascha Willems</a>) in my spare time.<br>
+				No profit is made from this data, nor is this data used in any commercial way and no personal data is transferred, stored or passed.<br><br>
+				If you want to contribute to the development, you can find the source code at <a href="https://github.com/SaschaWillems" target="_blank">https://github.com/SaschaWillems</a>.
+			</div>
+			<h2>Donating</h2>
+			<div>
+				All of my tools and the database itself are free-to-use, open source and hosted by me free of charge, feel free to donate ;)
+			</div>
+			<div>
+				<h3>Flattr</h3>
+				<script id='fbrtvnp'>(function(i){var f,s=document.getElementById(i);f=document.createElement('iframe');f.src='//api.flattr.com/button/view/?uid=swillems&button=compact&url='+encodeURIComponent(document.URL);f.title='Flattr';f.height=20;f.width=110;f.style.borderWidth=0;s.parentNode.insertBefore(f,s);})('fbrtvnp');</script>
+			</div>
+			<div>
+				<h3>PayPal</h3>
+				<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=BHXPMV6ZKPH9E"><img alt="" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif"/></a>
+			</div>				
+		</div>    			
+		</div>    
+	</div>
 </div>
 	
 </body>
