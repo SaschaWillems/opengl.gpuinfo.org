48db167db8b58adbcb3b9276ea6b985a4db49164
saschawillems <webmaster@saschawillems.de>
2015-10-30 21:00:05 +0100

    Downloadpage

---
+++ download.php
@@ -0,0 +1,49 @@
+<?php 	
+	/* 		
+	*
+	* OpenGL hardware capability database server implementation
+	*	
+	* Copyright (C) 2011-2015 by Sascha Willems (www.saschawillems.de)
+	*	
+	* This code is free software, you can redistribute it and/or
+	* modify it under the terms of the GNU Affero General Public
+	* License version 3 as published by the Free Software Foundation.
+	*	
+	* Please review the following information to ensure the GNU Lesser
+	* General Public License version 3 requirements will be met:
+	* http://www.gnu.org/licenses/agpl-3.0.de.html
+	*	
+	* The code is distributed WITHOUT ANY WARRANTY; without even the
+	* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+	* PURPOSE.  See the GNU AGPL 3.0 for more details.		
+	*
+	*/
+	include './gl_htmlheader.inc';	
+?>
+
+<div id='reportdiv'>	   
+	<div class="panel panel-default">
+		<div class="panel-body" style="margin-left:50px; width:65%px;">    
+			<h2>Downloads</h2>
+			
+<div class="bs-example">
+    <div class="list-group">
+        <a href="./downloads/win/glcapsviewer_1_1_win32.7z" class="list-group-item">
+			<h4 class="list-group-item-heading"><span class="glyphicon glyphicon-floppy-save"></span> Windows</h4>
+			<p class="list-group-item-text">Version 1.1 (32-Bit Executable, also works for 64-Bit)</p>
+        </a>
+        <a href="https://github.com/SaschaWillems/glCapsViewer" class="list-group-item">
+			<h4 class="list-group-item-heading">Linux</h4>
+			<p class="list-group-item-text">The linux release isn't available yet (I'm working on it). But feel free to clone from https://github.com/SaschaWillems/glCapsViewer.git and compile yourself using CMake and make. </p>
+        </a>
+        <a href="https://github.com/SaschaWillems/glCapsViewer" class="list-group-item">
+			<h4 class="list-group-item-heading">Mac OS</h4>
+			<p class="list-group-item-text">The Mac OS release isn't available yet (I'm working on it). But feel free to clone from https://github.com/SaschaWillems/glCapsViewer.git and compile yourself using CMake and make. </p>
+        </a>
+    </div>
+</div>
+
+</div>
+	
+</body>
+</html>
