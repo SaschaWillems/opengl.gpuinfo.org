18efb6f6bf1b7bbea651c65cf51b3cede12fc4b8
saschawillems <webmaster@saschawillems.de>
2018-05-20 23:06:16 +0200

    About page

---
+++ about.php
@@ -0,0 +1,55 @@
+<?php 	
+	/* 		
+	*
+	* OpenGL hardware capability database server implementation
+	*	
+	* Copyright (C) 2011-2018 by Sascha Willems (www.saschawillems.de)
+	*	
+	* This code is free software, you can redistribute it and/or
+	* modify it under the terms of the GNU Affero General Public
+	* License version 3 as published by the Free Software Foundation.
+	*	
+	* Please review the following information to ensure the GNU Lesser
+	* General Public License version 3 requirements will be met:
+	* http://www.gnu.org/licenses/agpl-3.0.de.html
+	*	
+	* The code is distributed WITHOUT ANY WARRANTY; without even the
+	* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+	* PURPOSE.  See the GNU AGPL 3.0 for more details.		
+	*
+	*/
+	include 'header.html';	
+?>
+
+<div id='reportdiv'>	   
+	<div class="panel panel-default">
+		<div class="panel-body" style="margin-left:50px; width:65%px;">    
+			<h2>About this database</h2>
+			<div>
+				Welcome to the public (inofficial) OpenGL hardware database, an online tool for developers that want to check out the OpenGL hardware capabilites for the different<br>
+				graphics cards, operating systems and driver versions out there.<br><br>
+				This database and the tools to fill it are developed and maintained by me (<a href="https://www.saschawillems.de/" target="_blank">Sascha Willems</a>) in my spare time.<br>
+				No profit is made from this data, nor is this data used in any commercial way and no personal data is transferred, stored or passed.<br><br>
+				If you want to contribute to the development, you can find the source code at <a href="https://github.com/SaschaWillems" target="_blank">https://github.com/SaschaWillems</a>.
+			</div>
+			<h2>Donating</h2>
+			<div>
+				All of my tools and the database itself are free-to-use, open source and hosted by me free of charge, feel free to donate ;)
+			</div>
+			<div>
+				<h3>PayPal</h3>
+				<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=BHXPMV6ZKPH9E"><img alt="" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif"/></a>
+			</div>				
+		</div>    			
+		</div>    
+	</div>
+</div>
+
+<center>
+<?php 
+	include "footer.html";
+?>
+</center>
+	
+</body>
+</html>
