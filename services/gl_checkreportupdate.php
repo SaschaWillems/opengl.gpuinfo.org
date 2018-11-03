bb3368aba32f9f90cde8dd24ebf630866e793698
saschawillems <webmaster@saschawillems.de>
2018-11-03 12:25:40 +0100

    Replace deprecated mysql calls with PDO

--- services/gl_checkreportupdate.php
+++ services/gl_checkreportupdate.php
@@ -3,7 +3,7 @@
 	*
 	* OpenGL hardware capability database server implementation
 	*	
-	* Copyright (C) 2011-2015 by Sascha Willems (www.saschawillems.de)
+	* Copyright (C) 2011-2018 by Sascha Willems (www.saschawillems.de)
 	*	
 	* This code is free software, you can redistribute it and/or
 	* modify it under the terms of the GNU Affero General Public
@@ -19,9 +19,9 @@
 	*
 	*/
 	
-	include './../gl_config.php';
+	include './../dbconfig.php';
 	
-	dbConnect();	
+	DB::connect();	
 
 	// Checks if the send report has fields missing in the online report
 	// If that's the case, the report can be updated
@@ -29,34 +29,37 @@
 	$capsXml = $_GET['caps'];	
 	$reportId = $_GET['reportId'];
 	$canUpdate = false;
-
 	$capsArray = explode(",", $capsXml);
-	
-	// Select report	
-	$sqlResult = mysql_query("SELECT * from openglcaps WHERE reportId = $reportId");
 
-	$colIndex  = 0;    
-	while($row = mysql_fetch_row($sqlResult)) {
-		foreach ($row as $data) {
-			$fieldname = mysql_field_name($sqlResult, $colIndex);		  
-			$fieldvalue = trim($data);
-			if (in_array($fieldname, $capsArray)) {
-				if (is_null($data)) {
-					$canUpdate = true;
-					break;
+	try {	
+		$stmnt = DB::$connection->prepare("SELECT * from openglcaps WHERE reportId = :reportid");
+		$stmnt->execute(["reportid" => $reportId]);			
+		$colIndex  = 0;    
+		while($row = $stmnt->fetch(PDO::FETCH_NUM)) {
+			foreach ($row as $data) {
+				$meta = $stmnt->getColumnMeta($colindex);
+				$fieldname = $meta["name"];
+				$fieldvalue = trim($data);
+				if (in_array($fieldname, $capsArray)) {
+					if (is_null($data)) {
+						$canUpdate = true;
+						break;
+					}
 				}
+				$colIndex++;
 			}
-			$colIndex++;
-		}
-	}	
-	
-	if ($canUpdate) {
-		header('HTTP/ 200 update_possible');
-		echo "true";
-	} else {
-		header('HTTP/ 200 report_same');
-		echo "false";
+		}			
+		if ($canUpdate) {
+			header('HTTP/ 200 update_possible');
+			echo "true";
+		} else {
+			header('HTTP/ 200 report_same');
+			echo "false";
+		}	
+	} catch (PDOException $e) {
+		header('HTTP/ 500 server error');
+		echo 'Server error: Could not check report!';
 	}
 
-	mysql_close();	 
+	DB::disconnect();	 
 ?>
